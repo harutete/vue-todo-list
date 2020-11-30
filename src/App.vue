5870738bc1affeb3d81024660e4feb9589126fbf
harutete <ai.takeuchi.10@gmail.com>
2020-12-01 00:41:13 +0900

    Add: タスクを追加するところまで作成

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <h1>Todo List</h1>
+    <div class="content-todo-input-wrap">
+      <div class="content-todo-input-item">
+        <input type="text" v-model="todoText" class="input-add-todo" />
+      </div>
+      <div class="content-todo-input-item">
+        <button type="button" @click="addTodoTask()" class="button-add-todo">
+          Add Todo
+        </button>
+      </div>
+    </div>
+    <ul class="list-todo">
+      <li v-for="(todo, index) in todoList.value" :key="`todo_${index}`">
+        {{ todo }}
+      </li>
+    </ul>
   </div>
-  <router-view />
 </template>
 
+<script lang="ts">
+import { ref, defineComponent, onMounted } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const todoList = ref<string[]>([]);
+    const todoText = ref('');
+    const addTodoTask = () => {
+      todoList.value.push(todoText.value.toString());
+    };
+    return {
+      todoList,
+      todoText,
+      addTodoTask
+    };
+  }
+});
+</script>
+
 <style lang="scss">
-#app {
+body {
+  background: #f7f3dc;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #724c25;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.content-todo-input-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: -10px;
+  .content-todo-input-item {
+    margin-right: 10px;
+  }
+}
+.input-add-todo {
+  display: inline-block;
+  border: 1px solid #724c25;
+  border-radius: 4px;
+  padding: 10px 15px;
+}
+.button-add-todo {
+  cursor: pointer;
+  display: inline-block;
+  background: #f0c060;
+  border: none;
+  border-radius: 4px;
+  color: #ffffff;
+  padding: 10px 15px;
+}
+.list-todo {
+  li {
+    margin-top: 10px;
   }
 }
 </style>
